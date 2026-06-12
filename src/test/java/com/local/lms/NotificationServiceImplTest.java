e53d61b9a0e8680ddf71fa66e098c4b609c1daf7
Gathiira <gathiiramwangi@gmail.com>
2026-06-12 08:04:52 +0300

    fix: notification tests

--- src/test/java/com/local/lms/NotificationServiceImplTest.java
+++ src/test/java/com/local/lms/NotificationServiceImplTest.java
@@ -8,12 +8,10 @@ import com.local.lms.repository.LoanRepository;
 import com.local.lms.repository.NotificationRepository;
 import com.local.lms.repository.NotificationTemplateRepository;
 import com.local.lms.service.impl.NotificationServiceImpl;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -32,12 +30,6 @@ class NotificationServiceImplTest {
     @Mock private NotificationTemplateRepository templateRepository;
     @Mock private NotificationRepository notificationRepository;
     @Mock private LoanRepository loanRepository;
-
-    @BeforeEach
-    void setup() {
-        ReflectionTestUtils.setField(notificationService, "mockEnabled", true);
-        ReflectionTestUtils.setField(notificationService, "emailEnabled", false);
-    }
 
     // -----------------------------
     // SEND NOTIFICATION - MOCK MODE
